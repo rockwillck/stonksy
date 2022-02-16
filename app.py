687d3f3442f0e9985a05f15d36576d8521e65ba2
rockwillck <38663064+rockwillck@users.noreply.github.com>
2022-02-15 21:51:00 -0600

    Mobile Friendly

--- app.py
+++ app.py
@@ -182,3 +182,7 @@ def page_not_found(e):
     return render_template('404.html'), 404
 
 app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
+
+if __name__=="__main__":
+    app.run(host=os.getenv('IP', '0.0.0.0'), 
+            port=int(os.getenv('PORT', 4444)))
